21b7794a8401cc74edad2e261fb87acc221d36f0
Wmz1024 <82153828+wmz1024@users.noreply.github.com>
2022-08-02 16:10:11 +0800

    u

--- assets/js/setting.js
+++ assets/js/setting.js
@@ -7,13 +7,13 @@ function search(api){
 var s = localStorage.getItem("hitokoto");
 if (s!=null){
     if(s==true){
-        document.getElementById("open_h").selected
+        document.getElementById("open_h").selected="selected"
     }
     if(s==false){
-        document.getElementById("close_h").selected
+        document.getElementById("close_h").selected="selected"
     }
 }
 else{
     localStorage.setItem("hitokoto", true);
-    document.getElementById("open_h").selected
+    document.getElementById("open_h").selected="selected"
 }
