ac59d35eb6e82e5805bb50b2d0d27b0152078d7e
Wmz1024 <82153828+wmz1024@users.noreply.github.com>
2022-08-02 13:29:21 +0800

    u

--- assets/js/setting.js
+++ assets/js/setting.js
@@ -4,3 +4,16 @@ function search(api){
     inst1.open();
     localStorage.setItem("search", api);
 }
+var s = localStorage.getItem("hitokoto");
+if (s!=null){
+    if(s==true){
+        document.getElementById("open_h").selected
+    }
+    if(s==false){
+        document.getElementById("close_h").selected
+    }
+}
+else{
+    localStorage.setItem("hitokoto", true);
+    document.getElementById("open_h").selected
+}
