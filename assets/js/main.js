4d836df4aa6886b0a06e7a0c0af36165267d8419
Wmz1024 <82153828+wmz1024@users.noreply.github.com>
2022-08-02 11:40:41 +0800

    u

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,9 @@
 var s = localStorage.getItem("search");
+if(s!=null){
 if (s == "bing") { document.getElementById("searchbox").action = "/assets/html/bing.html" }
 if (s == "baidu") { document.getElementById("searchbox").action = "/assets/html/baidu.html"}
 if (s == "google") { document.getElementById("searchbox").action = "/assets/html/google.html" }
 if (s == "duckduckgo") { document.getElementById("searchbox").action = "/assets/html/duckduckgo.html" }
-else{
+}else{
     document.getElementById("searchbox").action = "/assets/html/baidu.html" 
 }
